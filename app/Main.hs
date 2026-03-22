0e4a43a2a7bbee134f903ede3c8e0d95c07bd5c6
Klara Modin <klarasmodin@gmail.com>
2026-03-22 21:46:43 +0100

    add conditional core output

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,22 @@
 module Main (main) where
 
 import Options.Applicative
-import GHC.Core
+-- import GHC.Core
 import Util
 import IR
 
-data Arguments = Arguments {
-  inputFile :: String
-}
+data Arguments = Arguments
+  { inputFile :: String
+  , outputCore :: Bool
+  , outputIr :: Bool
+  }
 
 parseArguments :: Parser Arguments
 parseArguments =
   Arguments
     <$> strArgument (metavar "input")
+    <*> flag False True (long "output-core")
+    <*> flag True False (long "no-output-ir")
 
 argumentParser :: ParserInfo Arguments
 argumentParser =
@@ -24,12 +28,11 @@ main :: IO ()
 main = do
   arguments <- execParser argumentParser
   (core, dflags) <- compileToCore Nothing (inputFile arguments)
-  print $ translate core
-  -- putStrLn $ showPpr dflags core
-  -- _ <- traverse (putStrLn . showPpr dflags . f) core
-  -- _ <- traverse (putStrLn . showPpr dflags) core
+  if outputCore arguments
+    then putStrLn $ showPpr dflags core
+    else pure ()
+  let ir = translate core
+  if outputIr arguments
+    then print ir
+    else pure ()
   pure ()
-  where
-    f (NonRec _ e) = e
-    f (Rec _) = undefined
-    g (binds, body) = stripNArgs (fromIntegral $ length binds) body
