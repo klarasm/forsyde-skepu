8121fedd0220bdd73132f85ce2f4081712fcbc1b
Klara Modin <klarasmodin@gmail.com>
2026-03-24 14:38:22 +0100

    make vertices

--- src/IR.hs
+++ src/IR.hs
@@ -7,6 +7,7 @@
 
 module IR
   ( System (..)
+  , Process (..)
   , Vertex (..)
   , Edge (..)
   , Port (..)
@@ -14,7 +15,8 @@ module IR
   )
 where
 
-import Data.List (intercalate, sort)
+--import Data.List (intercalate, sort)
+import Data.List (sort)
 import Data.Maybe (mapMaybe)
 import GHC.Core
 import GHC.Core.Type
@@ -25,52 +27,74 @@ import GHC.Types.Var (varUnique)
 import GHC.Types.Name (isTupleTyConName)
 import GHC.Utils.Outputable (showPprUnsafe)
 
-type Id = Unique
+data Id
+  = None
+  | Unique Unique
+  | Assigned Int
+  deriving (Show)
 
 data System = System
-  { id :: Maybe Id
+  { id :: Id
+  , processes :: [Process]
   , vertices :: [Vertex]
   , edges :: [Edge]
-  -- , sigs :: [(CoreBndr, CoreExpr)]
-  , apps :: [(CoreExpr, [Var], [Var])]
   }
 instance  Show System where
   show System { id = i, .. } =
     "System("
     <> show i
-    <> ", " <> (intercalate "\n" $ map show vertices)
-    <> ", " <> show edges
-    -- <> ", " <> "{" <> intercalate ", " (map (\(a, _) -> (showPprUnsafe a)) sigs) <> "}"
-    <> ", " <> "{" <> intercalate ", " (map (\(a, b, c) -> "(" <> (showPprUnsafe a) <> ", " <> (showPprUnsafe b) <> ", " <> (showPprUnsafe c)) apps) <> "}"
+    <> ", " <> (unlines . map ("\t" <>) . lines . concat . map (("\n" <>) . show) $ processes)
+    <> ", " <> (unlines . map ("\t" <>) . lines . concat $ map (("\n" <>) . show) $ vertices)
+    <> ", " <> (unlines . map ("\t" <>) . lines . concat $ map (("\n" <>) . show) $ edges)
     <> ")"
 
-data Vertex = Vertex
-  { id :: !Id
-  , binder :: CoreBndr
-  , inputs :: [Port]
-  , outputs :: [Port]
+-- A process constructor applied to a function, but not connected in a network.
+data Process = Process
+  { binder :: Maybe CoreBndr
+  , inports :: [Port]
+  , outports :: [Port]
   , subsystem :: Maybe System
-  , function :: CoreExpr
-  , ty :: Type
+  , body :: CoreExpr
   }
-instance Show Vertex where
-  show Vertex {id = i, ..} =
-    "Vertex("
-      <> show i
-      <> ", " <> showPprUnsafe binder
-      <> ", inputs = " <> show inputs
-      <> ", outputs = " <> show outputs
+instance Show Process where
+  show Process { .. } =
+    "Process("
+      <> showPprUnsafe binder
+      <> ", inports = " <> show inports
+      <> ", outports = " <> show outports
       <> ", " <> show subsystem
-      -- <> ", " <> showPprUnsafe ty
-      -- <> ", {" <> showPprUnsafe function <> "}"
+      -- <> ", {" <> showPprUnsafe body <> "}"
       <> ")"
 
+-- A process connected in a network.
+-- It must therefore have at least an input and an output
+data Vertex = Vertex
+  { id :: Id
+  , process :: Either Var Process
+  , inputs :: [Var]
+  , outputs :: [Var]
+  }
+instance Show Vertex where
+  show Vertex { id = i, .. } =
+    "Vertex("
+    <> show i
+    <> ", " <> either showPprUnsafe show process
+    <> ", inputs = " <> showPprUnsafe inputs
+    <> ", outputs = " <> showPprUnsafe outputs
+    <> ")"
+
 data Edge = Edge
-  { id :: !Id
+  { binder :: !Var
   , source :: !Id
   , target :: !Id
   }
-  deriving (Show)
+instance Show Edge where
+  show Edge { .. } =
+    "Edge("
+    <> showPprUnsafe binder
+    <> ", " <> show source
+    <> ", " <> show target
+    <> ")"
 
 data Port = Port
   { ty :: Type
@@ -84,17 +108,13 @@ instance Show Port where
 translate :: CoreProgram -> System
 translate f =
   System
-  { id = Nothing
-  , vertices
+  { id = None
+  , processes
+  , vertices = []
   , edges = []
-  -- , sigs = []
-  , apps = []
   }
   where
-    vertices = filter nonemptyVertex . map makeVertex $ f
-    -- We only care about actual functions
-    nonemptyVertex Vertex { inputs, outputs } =
-      length inputs /= 0 || length outputs /= 0
+    processes = mapMaybe makeProcess $ f
 
 makePorts :: ([Type], [Type]) -> ([Port], [Port])
 makePorts (inty, outty) =
@@ -116,32 +136,53 @@ extractTypes acc = \case
       TyConApp v types | isTupleTyConName $ tyConName v -> reverse resacc <> types
       t -> reverse (t : resacc)
 
-makeVertex :: CoreBind -> Vertex
-makeVertex (Rec _) = undefined
-makeVertex (NonRec bind expr) =
-  Vertex
-    { id = varUnique bind
-    , binder = bind
-    , inputs = inports
-    , outputs = outports
-    , subsystem = translateExpr bind expr
-    , function = expr
-    , ty = varType bind
-    }
+-- | Make a process from a binding.
+-- A process should be self-contained, i.e. not have any communication outside
+-- of its arguments and return.
+makeProcess :: CoreBind -> Maybe Process
+makeProcess (Rec _) = Nothing -- disallow non-self-contained processes
+makeProcess (NonRec bind expr) =
+  -- A process needs both an input and an output. A function with just an
+  -- output is a value
+  if length inports /= 0 && length outports /= 0
+    then
+      Just
+        Process
+          { binder = Just bind,
+            inports,
+            outports,
+            subsystem = translateExpr (Just bind) expr,
+            body = expr
+          }
+    else Nothing
   where
     (inports, outports) = makePorts . extractTypes [] $ varType bind
 
-translateExpr :: CoreBndr -> CoreExpr -> Maybe System
+translateExpr :: Maybe CoreBndr -> CoreExpr -> Maybe System
 translateExpr bind expr = out
   where
-    -- (vertices, edges) = translateExpr' ([], []) expr
     (binds, sigs) = getSignals [] [] expr
     apps' = map (getApplication binds sigs) sigs
     apps = mapMaybe (resolveTuples apps') apps'
+    processes = getProcesses [] expr
+    vertices = map makeVertex apps
     out =
       if length sigs /= 0
-        then pure $ System {id = Just $ varUnique bind, vertices = [], edges = [], apps }
+        then pure $ System
+          { id = maybe None (Unique . varUnique) bind
+          , processes
+          , vertices
+          , edges = []
+          }
         else Nothing
+
+getProcesses :: [Process] -> CoreExpr -> [Process]
+getProcesses acc = \case
+  Lam _ e -> getProcesses acc e
+  Let bind expr -> case makeProcess bind of
+    Nothing -> getProcesses acc expr
+    Just proc -> getProcesses (proc : acc) expr
+  _ -> acc
 
 -- | Get the applied signals of a subsystem
 -- These will later be used to derive vertices and edges
@@ -150,9 +191,22 @@ getSignals ::
   [(CoreBndr, CoreExpr)] ->
   CoreExpr ->
   ([CoreBndr], [(CoreBndr, CoreExpr)])
-getSignals bindacc acc expr = case collectBinders expr of
-  (newbind, Let (NonRec b e) inExpr) -> getSignals (newbind <> bindacc) ((b, e) : acc) inExpr
-  (newbind, Let (Rec sigs) inExpr) -> getSignals (newbind <> bindacc) (sigs <> acc) inExpr
+-- getSignals bindacc acc expr = case collectBinders expr of
+getSignals bindacc acc = \case
+  -- A signal should be fully applied, i.e. it should not have any input argument
+  Let (NonRec b e) inExpr | length (fst $ extractTypes [] $ varType b) == 0
+    -> getSignals bindacc ((b, e) : acc) inExpr
+  -- Likely a process, handled separately
+  Let (NonRec _ _) inExpr | otherwise
+    -> getSignals bindacc acc inExpr
+  -- A Rec binding should not contain any process since they are supposed to be
+  -- self-contained. Therefore filter binds with inputs.
+  Let (Rec sigs) inExpr
+    -> getSignals bindacc ((filter ((0==) . length . fst . extractTypes [] . varType . fst) sigs) <> acc) inExpr
+  Lam a e ->
+    -- error $ showPprUnsafe a <> showPprUnsafe bindacc
+    getSignals (a : bindacc) acc e
+  -- NOTE: need to handle system output somehow
   _ -> (bindacc, acc)
 
 -- | Resolve an application to a process, inputs and (potentially tupled) output
@@ -173,6 +227,7 @@ getApplication binds allSigs (output, expr) = (proc, input, output, splitTuples)
         (inputs, Just arg, e)
       e -> (inputs, split, e)
 
+-- | Resolve tupled outputs to their actual output signals
 resolveTuples ::
   [(CoreExpr, [Var], Var, Maybe Var)] ->
   (CoreExpr, [Var], Var, Maybe Var) ->
@@ -192,3 +247,29 @@ resolveTuples apps (proc, inputs, output, _) = Just (proc, inputs, outputs)
       (Case _ _ _ ((Alt _ args (Var out)) : _)) ->
         lookup out (zip args (zip [0..] $ repeat var))
       _ -> Nothing
+
+makeVertex :: (CoreExpr, [Var], [Var]) -> Vertex
+makeVertex = \case
+  -- An application of a non-inline process
+  (Var bind, inputs, outputs) ->
+      Vertex
+      { id = None
+      , process = Left bind
+      , inputs
+      , outputs
+      }
+  -- An application of an inline process definition
+  (expr, inputs, outputs) ->
+    Vertex
+    { id = None
+    , process = Right $
+        Process
+        { binder = Nothing
+        , inports = map (Port . varType) inputs
+        , outports = map (Port . varType) outputs
+        , subsystem = translateExpr Nothing expr
+        , body = expr
+        }
+    , inputs
+    , outputs
+    }
