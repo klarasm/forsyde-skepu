0d5c33155a247f674df0b178a79adaa5762434d5
Klara Modin <klarasmodin@gmail.com>
2026-04-24 23:14:30 +0200

    add comb23 and comb32

--- src/Synthesis.hs
+++ src/Synthesis.hs
@@ -427,8 +427,50 @@ bodyToStatement inports outports = \case
     error $ "8App(" <> show (Direct v :: Id ()) <> "): " <> showPprUnsafe e
   App (App (App (App (App (App (App (Var v) _) _) _) _) _) _) e ->
     error $ "7App(" <> show (Direct v :: Id ()) <> "): " <> showPprUnsafe e
-  App (App (App (App (App (App (Var v) _) _) _) _) _) e ->
-    error $ "6App(" <> show (Direct v :: Id ()) <> "): " <> showPprUnsafe e
+  App (App (App (App (App (App (Var v) t1) t2) t3) t4) t5) e
+    | getOccString v == "comb23" ->
+        let (args, expr) = collectBinders e
+            argMap = makeMap args inports
+         in case expr of
+              App (App (App (App (App (App (Var v') te1) te2) te3) e1) e2) e3
+                | getOccString v' == "(,,)" ->
+                    let tout1 = exprToCType te1
+                        tout2 = exprToCType te2
+                        tout3 = exprToCType te3
+                        (cntr, init1, (_, ea1)) = exprToCExpr 0 FunArg argMap (map exprToCType [t1, t2]) tout1 inports outports e1
+                        (cntr1, init2, (_, ea2)) = exprToCExpr cntr FunArg argMap (map exprToCType [t1, t2]) tout2 inports outports e2
+                        (_, init3, (_, ea3)) = exprToCExpr cntr1 FunArg argMap (map exprToCType [t1, t2]) tout3 inports outports e3
+                     in ( FunArg
+                        , CIR.SScope $
+                            init1
+                              <> init2
+                              <> init3
+                              <> [ CIR.SAssign (CIR.EDereference . CIR.EVar $ ExId Output <> Ix 0) ea1
+                                 , CIR.SAssign (CIR.EDereference . CIR.EVar $ ExId Output <> Ix 1) ea2
+                                 , CIR.SAssign (CIR.EDereference . CIR.EVar $ ExId Output <> Ix 2) ea3
+                                 ]
+                        )
+              e' -> error . showPprUnsafe $ e'
+    | getOccString v == "comb32" ->
+        let (args, expr) = collectBinders e
+            argMap = makeMap args inports
+         in case expr of
+              App (App (App (App (Var v') te1) te2) e1) e2
+                | getOccString v' == "(,)" ->
+                    let tout1 = exprToCType te1
+                        tout2 = exprToCType te2
+                        (cntr, init1, (_, ea1)) = exprToCExpr 0 FunArg argMap (map exprToCType [t1, t2, t3]) tout1 inports outports e1
+                        (_, init2, (_, ea2)) = exprToCExpr cntr FunArg argMap (map exprToCType [t1, t2, t3]) tout2 inports outports e2
+                     in ( FunArg
+                        , CIR.SScope $
+                            init1
+                              <> init2
+                              <> [ CIR.SAssign (CIR.EDereference . CIR.EVar $ ExId Output <> Ix 0) ea1
+                                 , CIR.SAssign (CIR.EDereference . CIR.EVar $ ExId Output <> Ix 1) ea2
+                                 ]
+                        )
+              e' -> error . showPprUnsafe $ e'
+    | otherwise -> error $ "6App(" <> show (Direct v :: Id ()) <> "): " <> showPprUnsafe e
   App (App (App (App (App (Var v) t1) t2) t3) t4) e
     | getOccString v == "comb13" ->
         let (args, expr) = collectBinders e
