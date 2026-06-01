c1ad089ff23b13c55fd84bafec78d7c5b6454076
Klara Modin <klarasmodin@gmail.com>
2026-06-01 14:45:57 +0200

    also use derefTo on unary operators

--- src/ForSyDe/Atom/Synthesis/SkePU.hs
+++ src/ForSyDe/Atom/Synthesis/SkePU.hs
@@ -200,10 +200,12 @@ resolveOp ::
   (Int, [CStatement], (CType, CExpression))
 resolveOp tmpix stmts [(t1, expr1)] tout = \case
   "length" -> case t1 of
-    CIR.TConstructor _ _ -> (tmpix, stmts, (tout, CIR.ECallExpr (CIR.EMemberAccess expr1 (ExId $ Name "size")) []))
+    CIR.TConstructor _ _ -> (tmpix, stmts, (tout, CIR.ECallExpr (CIR.EMemberAccess e1 (ExId $ Name "size")) []))
     _ -> error $ show t1
-  "id" -> (tmpix, stmts, (tout, expr1))
+  "id" -> (tmpix, stmts, (tout, e1))
   u -> error $ "Unknown unary function: " <> u
+ where
+  e1 = derefTo tout t1 expr1
 resolveOp tmpix stmts [(t1, expr1), (t2, expr2)] tout = \case
   "+" -> (tmpix, stmts, (tout, CIR.EBinOp CIR.Add e1 e2))
   "*" -> (tmpix, stmts, (tout, CIR.EBinOp CIR.Multiply e1 e2))
